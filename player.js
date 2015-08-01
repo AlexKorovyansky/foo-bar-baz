0fda2fbcd704c266811fe6fdf2537d25d22c7fbc
Alexey Belozerov <asbelozerov@gmail.com>
2015-08-01 12:21:36 +0600

    Refactoring

--- player.js
+++ player.js
@@ -1,48 +1,71 @@
 var rainman = require('./rainman');
 
+/**
+ * Return from 0.0 to 1.0
+ * 0.0 - very bad hand
+ * 1.0 = awesome hand
+ */
 function _estimateHand(hole_cards) {
-  return 1.0;
+  return 0.5;
 };
 
+function _estimateAllCards(cards) {
+  return 0.5;
+}
+
 function _estimateState(game_state) {
-  var handEstimation = _estimateHand(game_state.players[game_state.in_action].hole_cards);
-  return handEstimation;
-};
+
+  if(!community_cards.community_cards || community_cards.community_cards.length == 0) {
+    // Without community cards
+    return 0.5;
+
+  } else {
+    // With community cards
+    var handEstimation = _estimateHand(game_state.players[game_state.in_action].hole_cards);
+    return handEstimation;
+  }
+}
+
+function _raise(game_state) {
+  return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
+}
 
 module.exports = {
 
   VERSION: "LeanNodeJS engineered player tuna",
 
-  /**
-   * Return from 0.0 to 1.0
-   * 0.0 - very bad hand
-   * 1.0 = awesome hand
-   */
-
-
   bet_request: function(game_state) {
     console.log(game_state);
     try {
+      var inPlayers = game_state.players.filter(function(player) {
+        return player.status != "out";
+      });
 
       var stateEstimation = _estimateState(game_state);
       if(stateEstimation == 0)
         return 0;
 
-      var inPlayers = game_state.players.filter(function(player) {
-        return player.status != "out";
-      });
+      if(stateEstimation == 1)
+        return 500;
 
-      if(game_state.current_buy_in >= 1000 && inPlayers.length <= 2) {
+      /*if(game_state.current_buy_in >= 1000 && inPlayers.length <= 2) {
         console.info("game_state.current_buy_in >= 1000 && inPlayers.length <= 2");
-        if(Math.random() > 0.5) {
+        if(Math.random() > 0.5 || stateEstimation == 1) {
           return 1000000000;
         } else {
           return 0;
         }
+      }*/
+
+      if(inPlayers.length > 2 && game_state.current_buy_in >= 1000) {
+        if(stateEstimation == 1)
+          return _raise(game_state);
+        else
+          return 0;
       }
 
       console.info("Default behaviour");
-      return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
+      return _raise(game_state);
     }
     catch(e) {
       console.error(e);
