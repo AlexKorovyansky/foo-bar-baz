cbef1327f0bba1f1e76fc0a3bf51c11f8c2ef91b
Alexey Belozerov <asbelozerov@gmail.com>
2015-08-01 11:05:35 +0600

    Not all in

--- player.js
+++ player.js
@@ -6,6 +6,10 @@ module.exports = {
   bet_request: function(game_state) {
     console.log(game_state);
     try {
+      if(game_state.current_buy_in >= 1000) {
+        return 0;
+      }
+
       return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
     }
     catch(e) {
