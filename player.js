c1be7ea0bc477c9d2b3f1839ec9e1b23d91855a8
Alexey Belozerov <asbelozerov@gmail.com>
2015-08-01 11:25:04 +0600

    info added

--- player.js
+++ player.js
@@ -1,21 +1,23 @@
-//var rainman = require('rainman-js');
+var rainman = require('./rainman');
 
 module.exports = {
 
-  VERSION: "LeanNodeJS engineered player",
+  VERSION: "LeanNodeJS engineered player tuna",
 
   bet_request: function(game_state) {
     console.log(game_state);
     try {
 
-      var countInPlayers = game_state.players.filter(function(player) {
+      var inPlayers = game_state.players.filter(function(player) {
         return player.status != "out";
       });
 
-      if(game_state.current_buy_in >= 1000 && countInPlayers.length <= 2) {
+      if(game_state.current_buy_in >= 1000 && inPlayers.length <= 2) {
+        console.info("game_state.current_buy_in >= 1000 && inPlayers.length <= 2");
         return 1000000000;
       }
 
+      console.info("Default behaviour");
       return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
     }
     catch(e) {
---
+++ rainman.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alexeybelozerov on 01/08/15.
+ */
+
+module.exports = {
+
+    rank: function(cards) {
+
+    }
+
+};
