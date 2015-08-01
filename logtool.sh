7b6b720567b77963ae212aca7dc093740962f205
over <overln2@gmail.com>
2015-08-01 12:45:34 +0600

    fix tools

--- logtool.sh
+++ logtool.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+if [[ "$#" = 1 ]]; then
+    wget http://lumber.leanpoker.org/logs/tournament/5592bba01c450b0003000003/team/poker-player-leannodejs/log/$1 --output-document=log.real
+    cat log.real
+    cat log.real | python replace2.py > parsed.log
+    exit
+fi
 wget http://lumber.leanpoker.org/logs/tournament/5592bba01c450b0003000003/team/poker-player-leannodejs/log --output-document=log.log
 link_raw=`tail -4 log.log | head -1`
 echo $link_raw
