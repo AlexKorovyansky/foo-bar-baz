0fc2796091343f00623aaf69f6dec8449ff45d72
Alexey Belozerov <asbelozerov@gmail.com>
2015-08-01 10:59:35 +0600

    Minimum raise amount

--- player.js
+++ player.js
@@ -4,7 +4,14 @@ module.exports = {
   VERSION: "LeanNodeJS engineered player",
 
   bet_request: function(game_state) {
-    return 10000000;
+    console.log(game_state);
+    try {
+      return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
+    }
+    catch(e) {
+      console.error(e);
+      return 1000000000;
+    }
   },
 
   showdown: function(game_state) {
