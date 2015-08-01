ff78bbfe8b1b85f9a02a795fcb667701b7bd21f2
over <overln2@gmail.com>
2015-08-01 12:34:30 +0600

    tools #2

---
+++ logtool.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+wget http://lumber.leanpoker.org/logs/tournament/5592bba01c450b0003000003/team/poker-player-leannodejs/log --output-document=log.log
+link_raw=`tail -4 log.log | head -1`
+echo $link_raw
+link=${link_raw//'<a href="'/''}
+l=`python replace.py $link`
+wget http://lumber.leanpoker.org/$l --output-document=log.real
+cat log.real | python replace2.py > parsed.log
