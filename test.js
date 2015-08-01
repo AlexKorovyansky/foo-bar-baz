1033fca646266890493a66ecb627bfb31347b103
over <overln2@gmail.com>
2015-08-01 14:31:07 +0600

    New test for community card

--- test.js
+++ test.js
@@ -63,3 +63,70 @@ console.log('bet ->', player.bet_request({
   minimum_raise: 10,
   bet_index: 6
 }));
+
+//test for community cards
+console.log('bet ->', player.bet_request({
+  tournament_id: '5592bba01c450b0003000003',
+  game_id: '55bc8362a9affd0003000103',
+  round: 3,
+  players: [{
+    name: 'Awesome Incredible Poker Bot',
+    stack: 1950,
+    status: 'active',
+    bet: 20,
+    version: 'Awesome Incredible Poker Bot',
+    id: 0
+  },
+    {
+      name: 'LeanNodeJS',
+      stack: 970,
+      status: 'active',
+      bet: 20,
+      hole_cards: [{rank: 'J', suit: 'hearts'}, {rank: 'K', suit: 'clubs'}],
+      version: 'LeanNodeJS fox',
+      id: 1
+    },
+    {
+      name: 'Boris',
+      stack: 1030,
+      status: 'folded',
+      bet: 0,
+      version: 'Boris Raiser v1.0',
+      id: 2
+    },
+    {
+      name: 'PythonPokerTeam',
+      stack: 2000,
+      status: 'folded',
+      bet: 10,
+      version: 'Inky 0.3',
+      id: 3
+    },
+    {
+      name: 'sevenbits',
+      stack: 0,
+      status: 'out',
+      bet: 0,
+      version: 'Crazy All In Bot',
+      id: 4
+    },
+    {
+      name: 'JBot',
+      stack: 0,
+      status: 'out',
+      bet: 0,
+      version: 'Avada Kedavara! v7',
+      id: 5
+    }],
+  small_blind: 10,
+  orbits: 0,
+  dealer: 2,
+  community_cards: [{rank: '6', suit: 'diamonds'},
+    {rank: '8', suit: 'diamonds'},
+    {rank: '2', suit: 'diamonds'}],
+  current_buy_in: 20,
+  pot: 50,
+  in_action: 1,
+  minimum_raise: 20,
+  bet_index: 4
+}));
