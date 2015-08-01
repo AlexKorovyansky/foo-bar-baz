bd8e405a58aa7eeee545239d251b828774e2b44d
over <overln2@gmail.com>
2015-08-01 14:15:52 +0600

    New test data

--- test.js
+++ test.js
@@ -1,64 +1,65 @@
 var player = require('./player');
-player.bet_request({
+console.log('bet ->', player.bet_request({
   tournament_id: '5592bba01c450b0003000003',
-  game_id: '55bc57aecc04560003000048',
-  round: 14,
+  game_id: '55bc7fdea9affd00030000dc',
+  round: 0,
   players: [{
     name: 'Awesome Incredible Poker Bot',
-    stack: 0,
-    status: 'out',
+    stack: 1000,
+    status: 'folded',
     bet: 0,
     version: 'Awesome Incredible Poker Bot',
     id: 0
   },
     {
       name: 'LeanNodeJS',
-      stack: 2235,
+      stack: 990,
       status: 'active',
-      bet: 30,
-      hole_cards: [Object],
-      version: 'LeanNodeJS engineered player',
+      bet: 10,
+      hole_cards: [{rank: 'K', suit: 'clubs'},
+        {rank: '8', suit: 'diamonds'}],
+      version: 'LeanNodeJS fox',
       id: 1
     },
     {
       name: 'Boris',
-      stack: 0,
-      status: 'out',
-      bet: 0,
+      stack: 980,
+      status: 'active',
+      bet: 20,
       version: 'Boris Raiser v1.0',
       id: 2
     },
     {
       name: 'PythonPokerTeam',
-      stack: 1010,
-      status: 'active',
-      bet: 30,
-      version: 'Inky 0.1',
+      stack: 1000,
+      status: 'folded',
+      bet: 0,
+      version: 'Inky 0.3',
       id: 3
     },
     {
       name: 'sevenbits',
-      stack: 1625,
-      status: 'active',
-      bet: 70,
-      version: 'Crazy Bot',
+      stack: 1000,
+      status: 'folded',
+      bet: 0,
+      version: 'Crazy All In Bot',
       id: 4
     },
     {
       name: 'JBot',
-      stack: 0,
-      status: 'out',
-      bet: 0,
-      version: 'Default Java folding player',
+      stack: 980,
+      status: 'active',
+      bet: 20,
+      version: 'Rom dom dom! v6',
       id: 5
     }],
   small_blind: 10,
-  orbits: 2,
-  dealer: 1,
+  orbits: 0,
+  dealer: 0,
   community_cards: [],
-  current_buy_in: 70,
-  pot: 130,
+  current_buy_in: 20,
+  pot: 50,
   in_action: 1,
-  minimum_raise: 40,
-  bet_index: 10
-});
+  minimum_raise: 10,
+  bet_index: 6
+}));
