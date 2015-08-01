144c38ed4f222f7ab97f42df2461d532a859ad5e
Alexey Belozerov <asbelozerov@gmail.com>
2015-08-01 11:15:48 +0600

    All in if last player all in

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+//var rainman = require('rainman-js');
 
 module.exports = {
 
@@ -6,8 +7,13 @@ module.exports = {
   bet_request: function(game_state) {
     console.log(game_state);
     try {
-      if(game_state.current_buy_in >= 1000) {
-        return 0;
+
+      var countInPlayers = game_state.players.filter(function(player) {
+        return player.status != "out";
+      });
+
+      if(game_state.current_buy_in >= 1000 && countInPlayers.length <= 2) {
+        return 1000000000;
       }
 
       return game_state.current_buy_in - game_state.players[game_state.in_action]["bet"] + game_state.minimum_raise;
